0857bc7228bac9e1f79dd046cf671eababb95310
Gabriel <gabrielmoses565@gmail.com>
2022-10-24 18:27:34 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let sx : [i32; 4] =[3 ,2, 4, 3];
+    let arr : [i32; 4] =[3 ,2, 4, 3];
     let target : i32 = 6;
     let mut count = 1;
-    for i in 0..sx.len() - 1{
-        for j in count..sx.len(){
-            if sx[i] + sx[j] == target{
+    for i in 0..arr.len() - 1{
+        for j in count..arr.len(){
+            if arr[i] + arr[j] == target{
                 println!("{} {} ", i, j);
                 count+= 1;
             }else{
